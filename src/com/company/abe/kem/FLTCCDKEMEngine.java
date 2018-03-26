bfa555842669c33fb90cbf4ae535a1a4c91b3202
andrluc <andrluc@amazon.com>
2018-03-26 13:41:42 +0300

    Added V_A computation for the decryption phase.

--- src/com/company/abe/kem/FLTCCDKEMEngine.java
+++ src/com/company/abe/kem/FLTCCDKEMEngine.java
@@ -1,12 +1,15 @@
 package com.company.abe.kem;
 
 import com.company.abe.parameters.*;
+import com.google.common.collect.Lists;
 import it.unisa.dia.gas.crypto.jpbc.kem.PairingKeyEncapsulationMechanism;
 import it.unisa.dia.gas.jpbc.Element;
+import it.unisa.dia.gas.plaf.jpbc.util.io.PairingStreamReader;
 import it.unisa.dia.gas.plaf.jpbc.util.io.PairingStreamWriter;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.List;
 
 public class FLTCCDKEMEngine extends PairingKeyEncapsulationMechanism {
     public FLTCCDKEMEngine() {
@@ -27,20 +30,53 @@ public class FLTCCDKEMEngine extends PairingKeyEncapsulationMechanism {
     }
 
     @Override
-    public byte[] process(byte[] bytes, int inOff, int inLen) {
+    public byte[] process(byte[] in, int inOff, int inLen) {
         String assignment;
         if (this.key instanceof FLTCCDDecryptionParameters) {
             FLTCCDDecryptionParameters decKey = (FLTCCDDecryptionParameters) this.key;
-            FLTCCDSecretKeyParameters sk = (FLTCCDSecretKeyParameters)decKey.getSecretKey();
+            FLTCCDSecretKeyParameters sk = decKey.getSecretKey();
             assignment = decKey.getAssignment();
+            PairingStreamReader reader = new PairingStreamReader(this.pairing, in, inOff);
+            List<Element> ts = Lists.newArrayList();
 
             assert assignment.length() == sk.getCircuit().getN();
 
             for (int i = 0; i < sk.getCircuit().getN(); i++) {
-//                sk.getParameters()
-//                        .getPairing()
-//                        .pairing()
+                if (assignment.charAt(i) == '1') {
+                    ts.add(reader.readG1Element());
+                } else {
+                    ts.add(null);
+                }
             }
+
+            List<List<Element>> vA = Lists.newArrayList();
+            for (int i = 0; i < sk.getCircuit().getN(); i++) {
+                if (assignment.charAt(i) == '1') {
+                    vA.add(Lists.newArrayList());
+                } else {
+                    vA.add(null);
+                    continue;
+                }
+
+                List<Element> di = sk.getDElementsAt(i);
+                for (int j = 0; j < di.size(); i++) {
+                    Element element1 = pairing
+                            .getG1()
+                            .newOneElement()
+                            .powZn(ts.get(i));
+                    Element element2 = pairing
+                            .getG2()
+                            .newOneElement()
+                            .powZn(sk.getDElementsAt(i).get(j));
+                    Element element = pairing
+                            .pairing(element1, element2);
+
+                    vA.get(i).add(element);
+                }
+            }
+
+
+
             return null;
         } else {
             FLTCCDEncryptionParameters encKey = (FLTCCDEncryptionParameters)this.key;
