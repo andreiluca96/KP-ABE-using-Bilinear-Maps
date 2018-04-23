8c804f70fccbdbe5143dfbb69435b0a91f6e829a
andrluc <andrluc@amazon.com>
2018-04-23 15:53:24 +0300

    Added Key exchange management engine encryption/decryption result. (for issue: https://github.com/andreiluca96/KP-ABE--Access-control-model-using-Bilinear-Maps/issues/1)

---
+++ src/com/company/abe/kem/results/FLTCCDKEMEngineEncryptionResult.java
@@ -0,0 +1,26 @@
+package com.company.abe.kem.results;
+
+import it.unisa.dia.gas.jpbc.Element;
+
+import java.util.List;
+
+public class FLTCCDKEMEngineEncryptionResult {
+    private Element ys;
+    private List<Element> e;
+
+    public Element getYs() {
+        return ys;
+    }
+
+    public void setYs(Element ys) {
+        this.ys = ys;
+    }
+
+    public List<Element> getE() {
+        return e;
+    }
+
+    public void setE(List<Element> e) {
+        this.e = e;
+    }
+}
